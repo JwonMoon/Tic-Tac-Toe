5bce74816415b262bf7f1e3a23c67656232fcc3a
JwontheMoon <mjiw9440@naver.com>
2022-02-15 15:55:31 +0900

    0215

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ BOARD_SIZE = 3
 # players
 P1 = 0
 P2 = 1
-NONE = 2
+P_NONE = 2
 # markers
 P1_M = 'O'
 P2_M = 'X'
 NONE_M = '_'
 
-def getPlayerName(player):
-    if player == P1:
+def getPlayerName(expr):
+    if expr == P1 or expr == 'O':
         return 'P1'
-    elif player == P2:
+    elif expr == P2 or expr == 'X':
         return 'P2'
-    else:
+    elif expr == P_NONE or expr == '_':
         return 'NONE'
+    else:
+        print("getPlayerName(expr): expr 인자를 잘못 입력하였습니다.", expr)
+        return -1
 
 def getPlayerMark(player):
     if player == P1:
@@ -48,69 +51,96 @@ def printBoard(BOARD):
 
 
 def isGameDone(BOARD):
-    winner = NONE
+    winner = P_NONE
     # 가로 검사
     for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE-1):
-            if BOARD[i][j] != NONE_M and BOARD[i][j] != BOARD[i][j+1]:
+        for j in range(BOARD_SIZE):
+            if BOARD[i][j] == NONE_M:
                 break
             elif j == BOARD_SIZE-1:
                 winner = BOARD[i][j]
+                print("[1] winner:", winner)
+                return winner
+            elif BOARD[i][j] != BOARD[i][j+1]:
+                break
 
     # 세로 검사
     for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE-1):
-            if BOARD[j][i] != NONE_M and BOARD[j][i] != BOARD[j+1][i]:
+        for j in range(BOARD_SIZE):
+            if BOARD[j][i] == NONE_M:
                 break
             elif j == BOARD_SIZE-1:
-                winner = BOARD[j][i]
+                    winner = BOARD[j][i]
+                    print("[2] winner:", winner)
+                    return winner
+            elif BOARD[j][i] != BOARD[j+1][i]:
+                break
                 
     # ↘ 대각선 검사
-    for i in range(BOARD_SIZE-1):
-            if BOARD[i][i] != NONE_M and BOARD[i][i] != BOARD[i+1][i+1]:
+    for i in range(BOARD_SIZE):
+            if BOARD[i][i] == NONE_M:
                 break
-            elif i == BOARD_SIZE-1:
+            elif i == BOARD_SIZE - 1:
                 winner = BOARD[i][i]
+                print("[3] winner:", winner)
+                return winner
+            elif BOARD[i][i] != BOARD[i+1][i+1]:
+                break
+
     # ↙ 대각선 검사
-    for i in range(BOARD_SIZE-1, 0):
-        if BOARD[i][i] != NONE_M and BOARD[i][i] != BOARD[i-1][i-1]:
+    for i in range(BOARD_SIZE):
+        if BOARD[i][BOARD_SIZE-i-1] == NONE_M:
             break
-        elif i == 1:
-            winner = BOARD[i][i]
-    
+        elif i == BOARD_SIZE - 1:
+            winner = BOARD[i][BOARD_SIZE-i-1]
+            print("[3] winner:", winner)
+            return winner
+        elif BOARD[i][BOARD_SIZE-i-1] != BOARD[i+1][BOARD_SIZE-i-2]:
+            break
     return winner
 
 
-def putMarker(player):
-    global BOARD
-    print("{} 차례입니다.".format(getPlayerName(player)))
+def inputRowCol(player):
+    print("[{} 차례입니다.] (게임종료: 0,0 입력)".format(getPlayerName(player)))
     row = int(input("행 번호: "))
     col = int(input("열 번호: "))
-    if(row > BOARD_SIZE or col > BOARD_SIZE or row*col < 0):
-        print("잘못 입력하였습니다.")
-        return False
+    if(row > BOARD_SIZE or col > BOARD_SIZE or row*col < 0): # 인덱스 범위 검사
+        print("번호를 잘못 입력하였습니다.\n")
+        return -1, -1
+    elif isEmpty(BOARD, row, col) == False: # 마커 중복 검사
+        print("빈칸이 아닙니다.\n")
+        return -1, -1
+    else:
+        return row, col
+
+def isEmpty(BOARD, row, col):
+    return (BOARD[row-1][col-1] == NONE_M)
+
+
+def putMarker(player, row, col):
+    global BOARD    
     BOARD[row-1][col-1] = getPlayerMark(player)
-    printBoard(BOARD)
-    print("")
     return True
 
 if __name__ == "__main__":
     BOARD = initBoard()
     player = P1
     
-    printBoard(BOARD)
-    print("")
     while(True):
-        winner = isGameDone(BOARD)
-        if(winner == NONE):
+        print("")
+        printBoard(BOARD)
+        print("")
+        winner = isGameDone(BOARD)  # 승패검사
+        if(winner == P_NONE):
             while(True):
-                if(putMarker(player)):
+                row, col = inputRowCol(player) # 숫자입력 예외 처리
+                if(row == 0 and col == 0):
+                    print("게임을 종료합니다.\n")
+                    exit()
+                elif(row != -1 and col != -1):
+                    putMarker(player, row, col) # 마커 입력
                     player = (player + 1) % 2   # player 변경
-                    break            
+                    break
         else:
-            print("<{}>가 이겼습니다!".format(getPlayerName(winner)))
+            print("@ {} @가 이겼습니다!".format(getPlayerName(winner)))
             break
-
-# 추가할 것
-# 1) 승패조건 보완
-# 2) 중복 마크 예외처리
